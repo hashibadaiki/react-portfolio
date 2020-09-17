5f8bac6fe59cae4540b03828d3c2523d307bb1f8
hashibadaiki <higemeganeking@gmail.com>
2020-09-17 14:35:24 +0900

    fix: header　fileを書き換えた

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import HeroImg from "./HeroImg";
 import Portfolio from "./Portfolio";
 import AboutMe from "./AboutMe";
 import AndMore from "./AndMore";
-import * as serviceWorker from "./serviceWorker";
 
 function Main() {
   return (
--- src/header.js
+++ src/header.js
@@ -1,28 +1,83 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import React from "react";
+
+const navigationWide = css`
+  display: inline-block;
+  width: 100%;
+  transform: scale(1, 1);
+  height: 80px;
+  background-color: rgba(12, 12, 11, 0.925);
+  color: rgb(238, 242, 245);
+  z-index: 10;
+`;
+
+const navigationList = css`
+  font-size: 2rem;
+  text-align: center;
+  display: none;
+`;
+
+const navigationList__portfolio = css`
+  padding-top: 150px;
+`;
+
+const navigationList__profile_contact_top = css`
+  padding-top: 50px;
+  list-style-type: none;
+`;
+
+const navigationList__btn = css`
+  display: inline-block;
+  cursor: pointer;
+  font-family: "Oswald", sans-serif;
+`;
 
 function Header() {
   return (
-    <nav class="navigation wide">
-      <ul class="navigationList" id="navigationWide__js">
-        <li class="navigationList__top">
-          <div class="navigationList__btn" id="topJs__wide">
+    <nav css={navigationWide} class="navigation wide">
+      <ul css={navigationList} class="navigationList" id="navigationWide__js">
+        <li
+          css={navigationList__profile_contact_top}
+          class="navigationList__top"
+        >
+          <div
+            css={navigationList__btn}
+            class="navigationList__btn"
+            id="topJs__wide"
+          >
             TOP
           </div>
         </li>
-        <li class="navigationList__portfolio">
-          <div class="navigationList__btn" id="portfolioJs__wide">
+        <li css={navigationList__portfolio} class="navigationList__portfolio">
+          <div
+            css={navigationList__btn}
+            class="navigationList__btn"
+            id="portfolioJs__wide"
+          >
             My portfolio
           </div>
         </li>
-        <li class="navigationList__profile">
-          <div class="navigationList__btn" id="aboutJs__wide">
+        <li
+          css={navigationList__profile_contact_top}
+          class="navigationList__profile"
+        >
+          <div
+            css={navigationList__btn}
+            class="navigationList__btn"
+            id="aboutJs__wide"
+          >
             About me
           </div>
         </li>
-        <li class="navigationList__contact">
-          <div class="navigationList__btn" id="contactJs__wide">
+        <li
+          css={navigationList__profile_contact_top}
+          class="navigationList__contact"
+        >
+          <div
+            css={navigationList__btn}
+            class="navigationList__btn"
+            id="contactJs__wide"
+          >
             and more...
           </div>
         </li>
