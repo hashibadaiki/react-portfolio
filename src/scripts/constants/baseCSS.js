0d64ac4bb990609c6435471b44a5ce5944fd3962
hashibadaiki <higemeganeking@gmail.com>
2020-10-02 12:32:50 +0900

    add: constantsディレクトリを追加した

---
+++ src/scripts/constants/baseCSS.js
@@ -0,0 +1,7 @@
+export const baseCSS = {
+  englishFont: '"Gill Sans", sans-serif',
+  pickUpColor: "rgb(20, 19, 19)",
+  backGroundColor: "rgba(12, 12, 11, 0.925)",
+  headFooterColor: "rgb(238, 242, 245)",
+  annotationColor: "rgb(129, 126, 126)",
+};
--- src/scripts/constants/resetCSS.js
+++ src/scripts/constants/resetCSS.js
@@ -1,13 +1,5 @@
 import { css } from "@emotion/core";
 
-export const baseCSS = {
-  englishFont: '"Gill Sans", sans-serif',
-  pickUpColor: "rgb(20, 19, 19)",
-  backGroundColor: "rgba(12, 12, 11, 0.925)",
-  headFooterColor: "rgb(238, 242, 245)",
-  annotationColor: "rgb(129, 126, 126)",
-};
-
 export const resetCSS = css`
   html,
   body,
